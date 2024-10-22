1cacd5941b847b1dbf3c282bf5c409f844ca6096
anthef <anthonyef09@gmail.com>
2024-10-22 21:38:08 +0700

    finish page register and login

--- Authenticate/urls.py
+++ Authenticate/urls.py
@@ -3,9 +3,10 @@ from Authenticate.views import *
 
 app_name = 'Authenticate'
 
-urlpatterns=[
-    path('login/',log_in,name='login'),
-    path('register/',register,name='register'),
-    path('logout/',log_out,name='logout'),
-    path('',log_in,name='login'),
+urlpatterns = [
+    path('login/', log_in, name='login'),  
+    path('register/', register, name='register'),  
+    path('logout/', log_out, name='logout'),  
+    # path('first-time-login/', first_time_login, name='first_time_login'), 
+    path('', log_in, name='login'), 
 ]

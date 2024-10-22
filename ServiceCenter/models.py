03f0ee9cf13f7eedbb27119f6bfd6bddc527750d
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-22 13:11:36 +0700

    made service center models

--- ServiceCenter/models.py
+++ ServiceCenter/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class ServiceCenter(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    name = models.CharField(max_length=255)
+    address = models.TextField()
+    contact = models.CharField(max_length=100)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    total_reviews = models.IntegerField()
+
+    def __str__(self):
+        return self.name

2b9fa65a483db15a41c0c579e3c55aba4abd80a4
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-22 22:46:57 +0700

    models for Tiket

--- Tiket/models.py
+++ Tiket/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from ServiceCenter.models import ServiceCenter
 
-# Create your models here.
+class Tiket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    service_center = models.ForeignKey(ServiceCenter, on_delete=models.CASCADE)
+    service_date = models.DateField()
+    service_time = models.TimeField()
+    specific_problems = models.TextField(default="No specific problems mentioned")
+
+    def __str__(self):
+        return self.name

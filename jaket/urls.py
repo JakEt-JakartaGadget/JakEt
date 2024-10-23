ce1858681d96636f493883627e909a7b76234008
Bertrand Gwynfory Iskandar <bertrand.gwynfory@gmail.com>
2024-10-23 14:47:43 +0700

    change setting for image

--- jaket/urls.py
+++ jaket/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for jaket project.
+URL configuration for bukulapak project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -14,13 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import include, path
 from django.contrib import admin
-from django.urls import path,include
-from jaket import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('authenticate/', include('Authenticate.urls')),
+    path('', include('main.urls')),
+    path('servicecenter/', include('ServiceCenter.urls')),
     path('tiket/', include('Tiket.urls')),
-    path('service-center/', include('ServiceCenter.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
